f94b94ec2c027ac344eac0563d73d7814419310d
Arnaud Wurmel <arnaud.wurmel1@gmail.com>
2024-06-20 22:18:38 +0200

    Add privacy policy file

--- sources/routes/index.ts
+++ sources/routes/index.ts
@@ -1,10 +1,11 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import Express, { Router, Request, Response, NextFunction } from 'express';
 import overviewRouter from './overview_router';
 import authenticationRouter from './authentication_router';
 import onboardingRouter from './onboarding_router';
 import sessionRouter from './session_router';
 import { authenticatedMiddleware } from '../middlewares/authenticated_middleware';
 import { APIError } from '../helpers/api_error';
+import path from "path";
 
 const router: Router = Router();
 
@@ -13,6 +14,7 @@ const router: Router = Router();
 */
 router.use("/auth", authenticationRouter)
 router.use("/onboarding", onboardingRouter)
+router.use('/resources', Express.static(path.join(__dirname, "../resources")));
 
 /*
 ** Private routes
